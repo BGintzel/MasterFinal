b4aa97ad3fb8b5ddbec8420bb321d5fe63a77c7b
BGintzel <b.gintzel@gmx.de>
2022-09-18 21:35:28 +0200

    fixed bugs

--- functions.py
+++ functions.py
@@ -3,11 +3,20 @@ import utils
 
 
 def run_training(args):
-    run = args[1]
+    run = args[2]
     net_name = 'EN' + run
     writer = utils.set_new_t_board(name=net_name)
     utils.save_file('stats/stats_' + run, trainrunner.trainer_EN(run, writer))
 
 
 def use_EN(args):
-    pass
+    device = utils.set_device()
+    path = args[2]
+
+    model = utils.load_model(path)
+
+    model.to(device)
+
+    img1_path = args[3]
+    img2_path = args[4]
+
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import sys
 from functions import run_training, use_EN
 
 if __name__ == '__main__':
+
     # make_new_experiment_arcs()
-    if sys.argv[0] == 'train':
+    if sys.argv[1] == 'train':
         run_training(sys.argv)
-    elif sys.argv[0] == 'use':
+    elif sys.argv[1] == 'use':
         use_EN(sys.argv)
